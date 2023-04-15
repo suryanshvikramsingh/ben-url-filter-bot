cda1500d4c754a8d73e3d14e4a8462f75c2332fa
Suryansh vikram singh <69345576+suryanshvikramsingh@users.noreply.github.com>
2023-04-15 11:41:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/dengerous53/ben-url-filter-bot.git /ben-url-filter-bot
+  git clone https://github.com/suryanshvikramsingh/ben-url-filter-bot /ben-url-filter-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone https://github.com/dengerous53/ben-url-filter-bot /ben-url-filter-bot
+  git clone https://github.com/suryanshvikramsingh/ben-url-filter-bot /ben-url-filter-bot
 fi
 cd /ben-url-filter-bot
 pip3 install -U -r requirements.txt
